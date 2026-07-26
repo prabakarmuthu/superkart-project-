b66d531d7aad86fb48e468adfa9f280e04efea61
Prabakar Muthukumarasamy <prabakarmuthu@gmail.com>
2026-07-27 01:25:36 +0800

    Add files via upload

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+model = joblib.load("SuperKart_prediction_model_v1_0.joblib")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = request.get_json()
+
+    df = pd.DataFrame([data])
+
+    prediction = model.predict(df)
+
+    return jsonify(
+        {
+            "predicted_sales": float(prediction[0])
+        }
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
